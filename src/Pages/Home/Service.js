353b5a1af04a7fcfecad8aa8c6ed19a8415a6d36
Fathir Ahmed Shishir <mfa.shishir@gmail.com>
2022-08-07 18:09:49 +0200

    appoinment page done

--- src/Pages/Home/Service.js
+++ src/Pages/Home/Service.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import fluoride from '../../assets/images/fluoride.png'
-import whitening from '../../assets/images/whitening.png'
-import cavity from '../../assets/images/cavity.png'
 
 const Service = ({service}) => {
-  
     return (
         <div className="card lg:max-w-lg bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
